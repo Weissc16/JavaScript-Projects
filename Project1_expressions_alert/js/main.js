85204e076843bd981a96d504056e3399a1658013
Cody Weiss <132945994+Weissc16@users.noreply.github.com>
2023-06-13 11:11:35 -0600

    JS Submission Assignment 1

---
+++ Project1_expressions_alert/js/main.js
@@ -0,0 +1,9 @@
+var sent1 = "This is the beginning of the string", sent2 = " and this is the end of the string"
+document.write(sent1 + sent2)
+//this is concatenating the variables sent1 and sent2 together//
+
+window.alert("what is the value of 3 + 3?")
+//this creates a pop up window stating "what is the value of 3 + 3?"//
+
+document.write( 3 + 3)
+//this is the expression giving the value of 3 + 3, but not sure how to get it it's own line just yet.//

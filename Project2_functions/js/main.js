e212aa41ef416aa1eb6a892c9a99f42c156f7ee9
Cody Weiss <132945994+Weissc16@users.noreply.github.com>
2023-06-15 10:59:28 -0600

    project2_functions assignment

---
+++ Project2_functions/js/main.js
@@ -0,0 +1,7 @@
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
+//+= concatenate's the sentence "I am learning" and " a lot from this book!"

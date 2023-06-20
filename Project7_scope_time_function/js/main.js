80b7c1b29c8384ef80724bb5a69ffb6e657c82e0
Cody Weiss <132945994+Weissc16@users.noreply.github.com>
2023-06-20 13:31:22 -0600

    Project7 fix

--- Project7_scope_time_function/js/main.js
+++ Project7_scope_time_function/js/main.js
@@ -67,3 +67,18 @@ function Color_Function() {
     document.getElementById("Your_color").innerHTML = Reply;
 }
 // Else/If statements within a function.  Using an input element and button.
+
+function Time_function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";
+    }
+    else if (Time >= 12 == Time < 18) {
+        Reply = "It is afternoon.";
+    }
+    else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}
---
+++ Project8_string_methods/js/main.js
@@ -0,0 +1,17 @@
+function full_Sentence() {
+    var part_1 = "I have ";
+    var part_2 = "made this ";
+    var part_3 = "into a complete ";
+    var part_4 = "sentence.";
+    var whole_sentence = part_1.concat(part_2, part_3, part_4);
+    document.getElementById("Concatenate").innerHTML = whole_sentence;
+}
+//using the concatenate method, we put all 4 of these variables together to create a sentence.
+
+function slice_Method() {
+    var Sentence = "All work and no play makes Johnny a dull boy.";
+    var Section = Sentence.slice(27,33);
+    document.getElementById("Slice").innerHTML = Section;
+}
+//using the slice method to cut characters 27 through 33 to display.  (Johnny)
+

ce32bd442b1d2ab8af49f004ffc8ebd37aabfe3b
Cody Weiss <132945994+Weissc16@users.noreply.github.com>
2023-06-20 17:20:13 -0600

    Project9 Assignment

--- Project9_countdown_slideshow/js/main.js
+++ Project9_countdown_slideshow/js/main.js
@@ -11,8 +11,8 @@ function currentSlide(n) {
 
 function showSlides(n) {
     let i;
-    let slides = document.getElementByClassName("mySlides");
-    let dots = document.getElementByClassName("dot");
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
